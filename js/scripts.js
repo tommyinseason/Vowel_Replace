c5b033ca6be12595131173a18a0e3be475077c74
john dow and tommy jones <john@>
2016-03-29 13:08:15 -0700

    spit out word puzzle

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
     $('#sentence').submit(function(event){
-
+      $(event.preventDefault());
       var sentenceString = $("#form1").val();
       var arraySplitString = sentenceString.split('');
 
       for (var index = 0; index <= arraySplitString.length; index += 1) {
-        if (arraySplitString[index] = "a","e","i","o","u") {
-          arraySplitString.replace('-');
-          alert(arraySplitString);
+        if (arraySplitString[index] === 'a', 'e', 'i', 'o', 'u') {
+          arraySplitString[index] = arraySplitString[index].replace(/a/gi,'-');
+          arraySplitString[index] = arraySplitString[index].replace(/e/gi,'-');
+          arraySplitString[index] = arraySplitString[index].replace(/i/gi,'-');
+          arraySplitString[index] = arraySplitString[index].replace(/o/gi,'-');
+          arraySplitString[index] = arraySplitString[index].replace(/u/gi,'-');
+          var smurf = arraySplitString.join('')
+          $('.output').text(smurf);
         }
-        }
-        alert(arraySplitString)
-      $(event.preventDefault());
-});
+      }
+  });
 });
